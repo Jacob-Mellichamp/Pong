ce72c1b8091910e49a338029e64030a6c8fbefbd
Zackary_Griffin <zackarygriffin30@gmail.com>
2020-05-02 23:26:58 -0400

    ball working

--- Ball.py
+++ Ball.py
@@ -12,21 +12,33 @@ The Player class will contain the methods:
 - move (self)
 - check (self)
 
-
 """
-import Pong
+import pygame
+from random import randint
+BLACK = (0,0,0)
 
-class Ball:
+class Ball(pygame.sprite.Sprite):
     
-    def __init__(self, x, y):
+    def __init__(self, color, x, y):
+        #calling the sprite constructor
+        super().__init__()
+
         self.x = x
         self.y = y
+        self.color = color
+        #create ball sprite
+        self.image = pygame.Surface([x,y])
+        #getting rectangle object
+        self.rect = self.image.get_rect()
+        #generating velocity
+        self.velocity = [randint(-8,8), randint(-8,8)]
 
-    def draw(self):
-        pass
-
-    def move(self):
-        pass
+    def move(self, speed):
+        self.rect.x += self.velocity[0] * speed
+        self.rect.y += self.velocity[1] * speed
+    
+    def draw(self, screen, spawnX, spawnY):
+         pygame.draw.rect(screen, self.color, [spawnX, spawnY, self.x, self.y])
 
     def check_if_hit(self):
         pass
