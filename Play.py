33c0206742b049e5782b32f37868dd8911b1cdd6
Jacob Mellichamp <jakemellichamp@gmail.com>
2020-05-02 22:56:23 -0400

    Player display

--- Play.py
+++ Play.py
@@ -1,20 +1,38 @@
+import pygame
 import Pong
 import Ball
 import Player
 
 
 
-def main():
-    #user is playing game
-    Playing = True
 
-    while Playing:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                Playing = False
+
+
+# Open a new window
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Pong")
+
+
+#draw net 
+pygame.draw.line(screen, Pong.WHITE, (350,500), (350, 0), 5)
+pygame.display.flip()
+
+
+#user is playing game
+Playing = True
+
+player1 = Player.Player(Pong.PLAYER1_START[0], Pong.PLAYER1_START[1])
+player2 = Player.Player(Pong.PLAYER2_START[0], Pong.PLAYER2_START[1])
+while Playing:
+    # draw rect
+    player1.draw(screen)
+    player2.draw(screen)
     
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Playing = False
+    pygame.display.update()
 
 
-
-            
-    pygame.quit()
+pygame.quit()
--- Pong.py
+++ Pong.py
@@ -8,33 +8,13 @@ WHITE = (255,255,255)
 #game options
 BALL_SPEED = 5
 AI_SPEED = 5
-PLAYER_WIDTH = 5
-PLAYER_HEIGHT = 50
 
-PLAYER1_START = (10,250)
-PLAYER2_START = (690, 250)
+#player options
+PLAYER_WIDTH = 10
+PLAYER_HEIGHT = 70
 
-
-# Open a new window
-size = (700, 500)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Pong")
-
-
-#draw net 
-pygame.draw.line(screen, WHITE, (350,500), (350, 0), 5)
-pygame.display.flip()
-
-#user is playing game
-Playing = True
-
-while Playing:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            Playing = False
-    
+PLAYER1_START = (20, 250)
+PLAYER2_START = (670, 250)
 
 
 
-            
-pygame.quit()
